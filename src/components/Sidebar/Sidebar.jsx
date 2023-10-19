bd25626e71af2b03f2f1a6bf15e89408b2f0eb5f
IgorMontezuma20 <igorminranda17@gmail.com>
2023-10-18 21:00:25 -0300

    criando o componente de Posts

--- src/components/Sidebar/Sidebar.jsx
+++ src/components/Sidebar/Sidebar.jsx
@@ -1,3 +1,5 @@
+import { PencilSimple } from 'phosphor-react'
+
 import styles from './Sidebar.module.css'
 
 export function Sidebar(){
@@ -7,12 +9,16 @@ export function Sidebar(){
             src="https://images.unsplash.com/photo-1515879218367-8466d910aaa4?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTB8fGNvZGV8ZW58MHx8MHx8fDA%3D&auto=format&fit=crop&w=500&q=50"/>
 
             <div className={styles.profile}>
+                <img className={styles.avatar} src="https://avatars.githubusercontent.com/u/85948415?v=4" />
                 <strong>Igor Montezuma</strong>
                 <span>FullStack Developer</span>
             </div>
 
            <footer>
-            <a href="#">Editar seu perfil</a>
+            <a href="#">
+                <PencilSimple size={20}/>
+                Editar seu perfil
+                </a>
            </footer>
         </aside>
     )
